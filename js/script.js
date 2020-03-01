d741398ca1de333b4555c6488c982a34a5688619
kakuyoto <56165783+kakuyoto@users.noreply.github.com>
2020-03-01 14:26:08 +0300

    Закончил js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
 var link = document.querySelector(".find-hotel");
 var popup = document.querySelector(".form-hotels");
-// var form = popup.querySelector("form");
-// var dateArrival = popup.querySelector("[name=date-arrival]");
-// var dateDeparture = popup.querySelector("[name=date-departure]");
-// var adults = popup.querySelector("[name=quantity-adults]");
-// var childrens = popup.querySelector("[name=quantity-children]");
+var form = popup.querySelector("form");
+var dateArrival = popup.querySelector("[name=date-arrival]");
+var dateDeparture = popup.querySelector("[name=date-departure]");
+var adults = popup.querySelector("[name=quantity-adults]");
+var childrens = popup.querySelector("[name=quantity-children]");
 
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("form-show");
-  // dateArrival.focus();
+  dateArrival.focus();
 });
 
-// form.addEventListener("submit", function (evt) {
-//   evt.preventDefault();
-// });
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+});
 
-// form.addEventListener("submit", function (evt) {
-//   if (!dateArrival.value || !dateDeparture.value || !adults.value || !childrens.value) {
-//     evt.preventDefault();
-//   }
-// });
+form.addEventListener("submit", function (evt) {
+  if (!dateArrival.value || !dateDeparture.value || !adults.value || !childrens.value) {
+    evt.preventDefault();
+  }
+});
 
-// window.addEventListener("keydown", function (evt) {
-//   if (evt.keyCode === 27) {
-//     evt.preventDefault();
-//     if (popup.classList.contains("form-show")) {
-//       popup.classList.remove("form-show");
-//     }
-//   }
-// });
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("form-show")) {
+      popup.classList.remove("form-show");
+    }
+  }
+});
+
+
+
