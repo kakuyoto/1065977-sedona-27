8f66d1cc557f13e4b397d510befba2052467b32a
kakuyoto <56165783+kakuyoto@users.noreply.github.com>
2020-03-03 13:51:01 +0300

    Надеюсь последние правки

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var link = document.querySelector(".find-hotel");
 var popup = document.querySelector(".form-hotels");
-// var form = popup.querySelector("form");
+var form = popup.querySelector("form");
 var dateArrival = popup.querySelector("[name=date-arrival]");
 var dateDeparture = popup.querySelector("[name=date-departure]");
 var adults = popup.querySelector("[name=quantity-adults]");
