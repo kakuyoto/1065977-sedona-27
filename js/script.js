e3b91a05027c5d1a050352269efe897461e26982
kakuyoto <56165783+kakuyoto@users.noreply.github.com>
2020-03-10 13:30:06 +0300

    сделал анимацию формы

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 var link = document.querySelector(".find-hotel");
 var popup = document.querySelector(".form-hotels");
-var close = document.querySelector(".form-map");
 var form = popup.querySelector(".serch-form");
 var dateArrival = popup.querySelector("[name=date-arrival]");
 var dateDeparture = popup.querySelector("[name=date-departure]");
@@ -13,15 +12,11 @@ link.addEventListener("click", function (evt) {
   dateArrival.focus();
 });
 
-close.addEventListener("click", function (evt) {
-  if (!popup.classList.contains(".form-remove")) {
-     popup.classList.add(".form-remove");
-  }
-});
 
-// popup.addEventListener("submit", function (evt) {
-//   evt.preventDefault();
-// });
+
+popup.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+});
 
 
 
